e67db3e37328c7a466bb15c913e9ffd742f3cf30
Stan Choi <sjc591@nyu.edu>
2025-01-22 16:49:13 -0500

    Update FPS

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
-        pygame.display.flip()        
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000      
 
 if __name__ == "__main__":
     main()
