8203a610abf7cb9865eafd9c7504c3d60bd0b667
Stan Choi <sjc591@nyu.edu>
2025-01-22 17:10:58 -0500

    Enable rotate left and rotate right

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ def main():
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
+
         screen.fill(color="black")
         player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000      
 
