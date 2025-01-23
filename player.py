d7f2c0cbf8d0180781504f8e9eb924b74171c904
Stan Choi <sjc591@nyu.edu>
2025-01-22 22:38:23 -0500

    Rename shoot timer variable

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.timer = 0
+        self.shoot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -25,7 +25,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
-        self.timer -= dt
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -37,7 +37,7 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            if self.timer <= 0:
+            if self.shoot_timer <= 0:
                 self.shoot()
 
     def move(self, dt):
@@ -47,4 +47,4 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        self.timer = PLAYER_SHOOT_COOLDOWN
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
